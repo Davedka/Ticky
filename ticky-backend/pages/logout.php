23462f021ac8ecacf4a32d2c6c5a49db67637902
Dávid <149328692+Davedka@users.noreply.github.com>
2026-05-11 12:30:11 +0200

    Update logout.php

--- ticky-backend/pages/logout.php
+++ ticky-backend/pages/logout.php
@@ -1,5 +1,9 @@
 <?php
 require_once __DIR__ . '/../utils/auth.php';
-ticky_logout();
+require_once __DIR__ . '/../utils/_nav.php';
+
+if (function_exists('ticky_logout')) {
+    ticky_logout();
+}
 header('Location: /login');
 exit;
